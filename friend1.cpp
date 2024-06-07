1dbb983da888bbb80c6c5f8be659e6f142c151d1
dharalunagariya <dharalunagariya5898@gmail.com>
2024-06-07 09:53:59 +0530

    changes

--- friend1.cpp
+++ friend1.cpp
@@ -12,32 +12,40 @@ class Number
         {
             num = n;
         }
-     friend int findlargest(Number n1 , Number n2);          
+     friend int findlargest(Number n1 , Number n2,Number n3);          
 };
-int findlargest(Number n1 , Number n2)
+int findlargest(Number n1 , Number n2 ,Number n3)
 {
-    if (n1.num > n2.num)
+    if (n1.num > n2.num && n1.num > n3.num)
     {
         return n1.num;
     }
-    else
+    else if (n1.num < n2.num && n2.num > n3.num)
     {
         return n2.num;
+    }
+    else
+    {
+       return n3.num;
     }
 }
 
 int main ()
 {
-    int nu1,nu2;
+    int nu1,nu2,nu3;
 
     cout << "Enter First Number : ";
     cin >> nu1;
     cout << "Enter Second Number : ";
     cin >> nu2;
+     cout << "Enter third Number : ";
+    cin >> nu3;
 
     Number n1(nu1);
     Number n2(nu2);
+    Number n3(nu3);
 
-    int largest = findlargest(n1, n2);
+
+    int largest = findlargest(n1, n2,n3);
     cout << "The largest number is: " << largest << endl;
 }
