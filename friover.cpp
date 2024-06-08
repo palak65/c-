e297f7b31f41013ef316729165af8aa182953ae2
dharalunagariya <dharalunagariya5898@gmail.com>
2024-06-08 09:36:57 +0530

    changes

---
+++ friover.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class box
+{
+    int l,w,h;
+    public:
+    void setdata(int a,int b,int c)
+    {
+        l=a,w=b,h=c;
+    }
+
+
+int getvolume()
+{
+    return l*w*h;
+}
+ friend box operator+(box m,box n)
+{
+    box t;
+    t.l=m.l-n.l;
+    t.w=m.w-n.w;
+    t.h=m.h-n.h;
+    return t;
+}
+ friend box operator++(box n,int)
+ {
+    box t;
+    t.l=++n.l;
+    t.w=++n.w;
+    t.h=++n.h;
+    return t;
+}
+};
+
+int main()
+{
+    box a,b,c,d;
+    a.setdata(3,3,3);
+    b.setdata(2,2,2);
+    cout<<"volume of A is:"<<a.getvolume()<<endl;
+    cout<<"volume of B is:"<<b.getvolume()<<endl;
+    c=a+b;
+     cout<<"volume of C is:"<<c.getvolume()<<endl;
+     d =c++;
+     cout<<"volume of D is:"<<d.getvolume()<<endl; 
+}
